1b9f4bdc032fb8068795c97df9aa098b3a59b7ca
Rafał Olszewski <37925892+rafalolszewski94@users.noreply.github.com>
2019-07-28 13:46:36 +0200

    Add index.html as content file
    Index html has body and html tag which are truncated by purgecss because it's not whitelisted.

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,8 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
   content: [
-    './src/**/*.html',
     './src/**/*.vue',
+    './index.html',
   ],
 
   whitelist: ['body', 'html', 'main'],
